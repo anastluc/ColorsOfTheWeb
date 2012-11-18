027007d3c38fec1bb98d67363c8c7a810aa9e8da
anastluc <anastluc@gmail.com>
2012-11-18 17:46:24 +0000

    method to find rgb histogram not just a histogram of each channel

--- src/main/java/com/anastluc/colorsoftheweb/Invocator.java
+++ src/main/java/com/anastluc/colorsoftheweb/Invocator.java
@@ -12,16 +12,33 @@ import com.anastluc.colorsoftheweb.image.Histogram;
  * @author lucas
  */
 public class Invocator {
-    public static void main(String[] args){
-        
+
+    public static void main(String[] args) {
+
         String testFile = "c:\\Selenium\\screenshot.png";
-        
+
         Histogram h = new Histogram(testFile);
-        long[][] bins = h.calculateBins();
-        DatabaseManager dm = new DatabaseManager();
-//        dm.test();
-        
-        
-        dm.storeImageFreqsToDb("no image storing at the moment - dull value", bins, h.getTotalPixels());
+
+
+//        long[][] bins = h.calculateBins();
+//        DatabaseManager dm = new DatabaseManager();
+//                dm.test();
+//        dm.storeImageFreqsToDb("no image storing at the moment - dull value", bins, h.getTotalPixels());
+
+        long[][][] rgbBin = h.calculateRGBBins();
+
+        for (int i = 0; i < 16; i++) {
+            for (int j = 0; j < 16; j++) {
+                for (int k = 0; k < 16; k++) {
+                    System.out.println("["+i+","+j+","+k+"]:"+rgbBin[i][j][k]);
+                }
+            }
+        }
+
+
+
+
+
+
     }
 }
--- src/main/java/com/anastluc/colorsoftheweb/image/Histogram.java
+++ src/main/java/com/anastluc/colorsoftheweb/image/Histogram.java
@@ -63,5 +63,28 @@ public class Histogram {
         return bi.getHeight()*bi.getWidth();
     }
     
-    
+    public long[][][] calculateRGBBins(){
+        int height = bi.getHeight();
+        int width  = bi.getWidth();
+        Raster raster = bi.getRaster();
+        long[][][] rgbBin = new long[16][16][16];//4096 possible colors
+        
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                int r_value = raster.getSample(i,j,0);
+                int g_value = raster.getSample(i,j,1);
+                int b_value = raster.getSample(i,j,2);               
+                
+                // x_value is between 0 and 255 (8bit channel)
+                // this leads to 16777216 possible different colors                
+                //reduce this to 4096 
+                r_value = (int)Math.floor(r_value / 16);
+                g_value = (int)Math.floor(g_value / 16);
+                b_value = (int)Math.floor(b_value / 16);
+                
+                rgbBin[r_value][g_value][b_value]++;
+            }            
+        }        
+        return rgbBin;
+    }        
 }
